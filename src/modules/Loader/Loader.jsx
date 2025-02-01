de7528c81424ea685f9df74b3566ae35588eb038
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2025-02-01 20:17:22 -0300

    Add Loader.jsx and styles

---
+++ src/modules/Loader/Loader.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import styles from "./Loader.module.css";
+import logo from "../../assets/image/logo.svg";
+
+const Loader = ({ onComplete }) => {
+  const [fadeOut, setFadeOut] = useState(false);
+  //logica de loader
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFadeOut(true);
+      // deja 1 segundo el fadeOut antes de cambiar el estado de isLoading
+      setTimeout(onComplete, 2000);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [onComplete]);
+
+  return (
+    <div className={`${styles.loader} ${fadeOut ? "invisible" : "visible"}`}>
+      <img src={logo} alt="logo" width={100} className={styles.loader__logo} />
+      <div className={styles.loader__content}>
+        <span className={styles.loader__name}>Fernando Zárate</span>
+        <span className={styles.loader__subtitle}>Portfolio</span>
+      </div>
+    </div>
+  );
+};
+export default Loader;
