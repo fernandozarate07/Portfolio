eb77d7134d653c3a6df0c8688a1b630793abfd24
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2025-02-02 14:01:36 -0300

    Add PrimaryBtn and styles

--- src/modules/Buttons/Available/AvailableBtn.jsx
+++ src/modules/Buttons/Available/AvailableBtn.jsx
@@ -1,47 +1,11 @@
-import { NavLink } from "react-router-dom";
-import styles from "./Buttonprimary.module.css";
+import styles from "./Cta.module.css";
 
-const Buttonprimary = ({ type, content, section, link, action }) => {
-  const handleClick = (e) => {
-    // Lógica para abrir enlaces de tipo mailto en ventana emergente
-    if (type === "link" && link.includes("mailto:")) {
-      e.preventDefault();
-      const width = 600;
-      const height = 600;
-      const left = (window.innerWidth - width) / 2;
-      const top = (window.innerHeight - height) / 2;
-      window.open(link, "_blank", `width=${width},height=${height},top=${top},left=${left}`);
-    }
-  };
-
-  if (type === "navlink") {
-    // Renderiza NavLink
-    return (
-      <NavLink to={section} className={styles.button}>
-        <span>{content}</span>
-      </NavLink>
-    );
-  }
-
-  if (type === "link") {
-    // Renderiza un enlace externo
-    return (
-      <a href={link} className={styles.button} target="_blank" rel="noreferrer" onClick={handleClick}>
-        {content}
-      </a>
-    );
-  }
-
-  if (type === "button") {
-    // Renderiza un botón
-    return (
-      <button className={styles.button} onClick={action}>
-        {content}
-      </button>
-    );
-  }
-
-  return null;
+const AvailableBtn = ({ link, content }) => {
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer" className={styles.button}>
+      {content}
+    </a>
+  );
 };
 
-export default Buttonprimary;
+export default AvailableBtn;
---
+++ src/modules/Buttons/Primary/PrimaryBtn.jsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Buttonprimary.module.css";
+
+const PrimaryBtn = ({ type, content, section, link, action }) => {
+  const handleClick = (e) => {
+    // Lógica para abrir enlaces de tipo mailto en ventana emergente
+    if (type === "link" && link.includes("mailto:")) {
+      e.preventDefault();
+      const width = 600;
+      const height = 600;
+      const left = (window.innerWidth - width) / 2;
+      const top = (window.innerHeight - height) / 2;
+      window.open(link, "_blank", `width=${width},height=${height},top=${top},left=${left}`);
+    }
+  };
+
+  if (type === "navlink") {
+    // Renderiza NavLink
+    return (
+      <NavLink to={section} className={styles.button}>
+        <span>{content}</span>
+      </NavLink>
+    );
+  }
+
+  if (type === "link") {
+    // Renderiza un enlace externo
+    return (
+      <a href={link} className={styles.button} target="_blank" rel="noreferrer" onClick={handleClick}>
+        {content}
+      </a>
+    );
+  }
+
+  if (type === "button") {
+    // Renderiza un botón
+    return (
+      <button className={styles.button} onClick={action}>
+        {content}
+      </button>
+    );
+  }
+
+  return null;
+};
+
+export default PrimaryBtn;
