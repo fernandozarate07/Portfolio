279cdda3b4b696d52a3cf17dde520ee89a67905d
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2025-02-02 14:18:59 -0300

    Add ProjectSection.jsx, Projec.jsx and styles

---
+++ src/modules/Sections/Projects/Project/Project.jsx
@@ -0,0 +1,57 @@
+import styles from "./Project.module.css";
+import PrimaryBtn from "../Buttons/Primary/PrimaryBtn.jsx";
+
+const Project = ({ title, description, imageUrl, nextProject, prevProject, isPaused, linkApp, linkCode }) => {
+  return (
+    <div className={styles.project}>
+      {/* btn up */}
+      <button onClick={prevProject} className={styles.project__projectBtn}>
+        <i className="fa-solid fa-arrow-left"></i>
+      </button>
+      {/* Container */}
+      <div className={styles.project__container}>
+        {/* img container */}
+        <div className={styles.project_imgContainer}>
+          {/* img content */}
+          <div className={styles.project__stopContainer}>
+            {isPaused === true && (
+              <div className={styles.project__stopBtn}>
+                <i className="fa-solid fa-pause"></i>
+              </div>
+            )}
+            <div className={styles.project__img}>
+              <img src={imageUrl} alt={title} width={300} />
+            </div>
+          </div>
+        </div>
+        {/* info */}
+        <div className={styles.project__infoSection}>
+          <div className={styles.project__titleContainer}>
+            <a href={linkApp} className={styles.project__title} target="_blank" rel="noopener noreferrer">
+              {title}
+            </a>
+            <a href={linkCode} className={styles.project__basecode} target="_blank" rel="noopener noreferrer">
+              Code
+            </a>
+          </div>
+          <div className={styles.project__descriptionContainer}>
+            <p className={styles.project__description}>{description[0]}</p>
+            <p className={styles.project__description}>{description[1]}</p>
+            <p className={styles.project__description}>{description[2]}</p>
+            <p className={styles.project__description}>{description[3]}</p>
+          </div>
+          <div className={styles.project__opcion}>
+            <PrimaryBtn type="link" content="Contáctame" link="mailto:fernandozarate.dev@gmail.com?subject=Hola Fer!" />
+            <PrimaryBtn type="navlink" content={"Dercargar CV"} section={"/aboutme"} />
+          </div>
+        </div>
+      </div>
+      {/* btn down */}
+      <button onClick={nextProject} className={styles.project__projectBtn}>
+        <i className="fa-solid fa-arrow-right"></i>
+      </button>
+    </div>
+  );
+};
+
+export default Project;
---
+++ src/modules/Sections/Projects/ProjectSection.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import styles from "./ProjectSection.module.css";
+import Project from "./Project/Project,jsx";
+
+import imgOne from "../../assets/image/one.svg";
+import imgTwo from "../../assets/image/two.svg";
+import imgThree from "../../assets/image/three.svg";
+
+const ProjectSection = () => {
+  const projects = [
+    {
+      imageUrl: imgOne,
+      title: "Truetone",
+      description: [
+        "Es una aplicación de afinador sencilla y precisa para guitarra, bajo y ukelele.",
+        "Desarrollada como una Single Page Application (SPA) con React, aprovecha la Web Audio API para capturar y procesar el sonido en tiempo real. Su diseño completamente responsive garantiza una experiencia óptima en cualquier dispositivo.",
+        "Tecnologías: HTML ~ CSS ~ React ~ Vite ~ Web Audio API",
+      ],
+      linkApp: "https://tuner-app-mu.vercel.app/",
+      linkCode: "https://github.com/fernandozarate07/tuner-app",
+    },
+    {
+      imageUrl: imgTwo,
+      title: "Simther",
+      description: [
+        "Es una aplicación web que permite conocer el clima de cualquier ciudad con solo unos clics. Utiliza la API de Visual Crossing para proporcionar datos precisos y actualizados del clima.",
+        "La app está desarrollada con una arquitectura VCM (Vista-Controlador-Modelo), lo que garantiza una estructura ordenada y eficiente. La lógica de programación y el código de renderizado están perfectamente separados y se conectan a través del Controlador, que gestiona la interacción con el DOM.",
+        "Tecnologias: HTML ~ CSS ~ Vanilla JS ~ WebPack",
+      ],
+      linkApp: "https://simple-weather-omega.vercel.app/",
+      linkCode: "https://github.com/fernandozarate07/weather-app",
+    },
+    {
+      imageUrl: imgThree,
+      title: "Protoprogramadores",
+      description: [
+        "Protoprogramadores es una agencia digital especializada en la creación y mantenimiento de sitios web profesionales para negocios y emprendedores.",
+        "Su plataforma fue desarrollada con WordPress e incorpora WooCommerce para gestionar la tienda online de manera eficiente.",
+        "Tecnologías:  HTML ~ CSS ~ React ~ WordPress ~ WooCommerce ~ Rank Math",
+      ],
+      linkApp: "https://protoprogramadores.com",
+      linkCode: "none",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+
+  useEffect(() => {
+    if (isPaused) return;
+
+    // cambia el intervalo a los 3s
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex === projects.length - 1 ? 0 : prevIndex + 1));
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentIndex, isPaused]);
+
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === projects.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1));
+  };
+
+  return (
+    <div
+      className={styles.projectSection}
+      onMouseEnter={() => setIsPaused(true)}
+      onMouseLeave={() => setIsPaused(false)}>
+      <Project
+        title={projects[currentIndex].title}
+        description={projects[currentIndex].description}
+        imageUrl={projects[currentIndex].imageUrl}
+        nextProject={nextProject}
+        prevProject={prevProject}
+        isPaused={isPaused}
+        linkApp={projects[currentIndex].linkApp}
+        linkCode={projects[currentIndex].linkCode}
+      />
+    </div>
+  );
+};
+
+export default ProjectSection;
